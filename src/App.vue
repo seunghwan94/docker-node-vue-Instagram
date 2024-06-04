bb25efffb6e744e98be4bb40a75851822d490157
sounghan94@gmail.com <sounghan94@gmail.com>
2024-06-04 11:32:17 +0000

    vue start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="contain">
+      <img alt="Vue logo" src="./assets/test.png">
+      <div class="login">
+        <div class="login-group">
+            <span>ID</span>
+            <input v-model="id" type="text" class="login-id" placeholder="UserID" aria-label="UserID">
+        </div>
+        <div class="login-group">
+            <span>PW</span>
+            <input v-model="pw" type="password" class="login-pw" placeholder="UserPW" aria-label="UserPW">
+        </div>
+        <div class="login-group">
+            <button @click="loginck">로그인</button>
+        </div>
+      </div>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+body{
+  margin:0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.contain {
+  margin:20px;
+  border: 2px solid gray;
+  border-radius: 15px;
+  height: calc(93vh - 44px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.contain img {
+  width: 25%;
+}
+
+
+
+.login-group {
+    display: flex;
+    align-items: center;
+    margin: 5px;
+    width: 100%; /* 부모 요소의 너비에 맞게 조정 */
+    max-width: 300px; /* 최대 너비 설정 */
+    justify-content: flex-end;
+        color: #fff;
+}
+.login-group span {
+    background: gray;
+    padding: 5px;
+    font-size: 17px;
+    border-radius: 5px 0 0 5px;
+    display: inline-block;
+    width: 50px;
+    text-align: center;
+    font-weight: bold;
+}
+.login-group input {
+    padding: 5px;
+    font-size: 17px;
+    border-radius: 0 5px 5px 0;
+    border: 1px solid transparent;
+    width: calc(100% - 50px - 2px); /* 부모 요소의 너비에서 span 너비와 여백을 뺀 값 */
+    margin-left: -1px; /* 테두리가 겹치지 않도록 조정 */
+}
+.login-group button {
+    width: 100%;
+    font-size: 17px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: 1px solid transparent;
+    cursor: pointer; /* 커서 변경 */
+    background:  gray;
+    color: #fff;
+    font-weight: bold;
+    margin-top: 20px;
 }
 </style>
